82842829d8a217729ee5c8dd1bb0ad8ed4c4288f
Member Lastname <member@0zxc.com>
2014-11-07 09:58:40 +0100

    Lorem ipsum dolor sit amet.

--- peopleModel.php
+++ peopleModel.php
@@ -32,6 +32,48 @@ class peopleModel extends Model {
     return $db->num_rows($res) != 0 ? true : 0;
   }
   
+  // if extended = true, it also queries all child tables
+  // defaults to false since its a hell of a presure on the database.
+  // remove once we add some proper caching
+  public function load_get_person($id, $extended = false) {
+    global $db;
+    $this->add_dependency('people', $id);
+    $id = $db->addslashes($id);
+    $res = $db->query("select * from persons where id = $id");
+    if (! $db->num_rows($res)) {
+      throw new Exception("Invalid person");
+    }
+    $person = $db->fetch_array($res, MYSQLI_ASSOC);
+    //TODO missing : person_languages_spoken, need to add table with ISO 639-1 codes
+    $tables_addresses = array('person_addresses', 'person_current_location');
+    $tables_organizations = array('person_jobs', 'person_schools');
+    $tables = array('person_activities', 'person_body_type', 'person_books', 'person_cars',
+        'person_emails', 'person_food', 'person_heroes', 'person_movies',
+        'person_interests', 'person_music', 'person_phone_numbers', 'person_quotes',
+        'person_sports', 'person_tags', 'person_turn_offs', 'person_turn_ons',
+        'person_tv_shows', 'person_urls');
+    foreach ($tables as $table) {
+      $person[$table] = array();
+      $res = $db->query("select * from $table where person_id = $id");
+      while ($data = $db->fetch_array($res, MYSQLI_ASSOC)) {
+        $person[$table][] = $data;
+      }
+    }
+    foreach ($tables_addresses as $table) {
+      $res = $db->query("select addresses.* from addresses, $table where $table.person_id = $id and addresses.id = $table.address_id");
+      while ($data = $db->fetch_array($res)) {
+        $person[$table][] = $data;
+      }
+    }
+    foreach ($tables_organizations as $table) {
+      $res = $db->query("select organizations.* from organizations, $table where $table.person_id = $id and organizations.id = $table.organization_id");
+      while ($data = $db->fetch_array($res)) {
+        $person[$table][] = $data;
+      }
+    }
+    return $person;
+  }
+
   public function remove_friend($person_id, $friend_id) {
     global $db;
     $this->invalidate_dependency('people', $person_id);
@@ -40,6 +82,18 @@ class peopleModel extends Model {
     $friend_id = $db->addslashes($friend_id);
     $res = $db->query("delete from friends where (person_id = $person_id and friend_id = $friend_id) or (person_id = $friend_id and friend_id = $person_id)");
     return $db->affected_rows($res) != 0;
+  }
+  
+  public function load_get_friend_requests($id) {
+    global $db;
+    $this->add_dependency('friendrequest', $id);
+    $requests = array();
+    $friend_id = $db->addslashes($id);
+    $res = $db->query("select person_id from friend_requests where friend_id = $friend_id");
+    while (list($friend_id) = $db->fetch_row($res)) {
+      $requests[$friend_id] = $this->get_person($friend_id, false);
+    }
+    return $requests;
   }
 
   public function set_profile_photo($id, $url) {
@@ -74,5 +128,15 @@ class peopleModel extends Model {
       $db->query($query);
     }
   }
-
+  
+  public function search($name) {
+    global $db;
+    $name = $db->addslashes($name);
+    $ret = array();
+    $res = $db->query("select id, email, first_name, last_name from persons where concat(first_name, ' ', last_name) like '%$name%' or email like '%$name%'");
+    while ($row = $db->fetch_array($res, MYSQLI_ASSOC)) {
+      $ret[] = $row;
+    }
+    return $ret;
+  }
 }
